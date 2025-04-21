b8c1ae123a1c2c48b860c959910dbbee2e665346
prajwallshetty <prajwal0shetty11@gmail.com>
2025-04-21 20:44:00 +0530

    routed

--- master/routes/web.php
+++ master/routes/web.php
@@ -11,6 +11,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('seller/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('seller');
+
+Route::get('admin/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('admin');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
